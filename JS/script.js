ef7ebec76ee2e1bfc063c68e4cccc388b598b029
Juliana Mendes <jmendxx@gmail.com>
2025-10-11 12:06:13 -0300

    Inclusão do envio do formulário pelo EmailJS

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,23 @@
+// Envio de formulario
+const form = document.querySelector('#form');
 
+emailjs.init("ckEgYeRX_wlZ02S-J");
 
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
 
+    emailjs.sendForm("service_ejcwzts", "template_portfolio", form)
+
+    .then(function() {
+        alert("Mensagem enviada com sucesso! 😄");
+        form.reset();
+    }, function(error) {
+        console.log(error);
+        alert("Erro ao enviar mensagem 😢 Tente novamente.")
+    });
+    
+   console.log('Passei por aqui');  
+}); 
 
 // Menu Mobile
 const btnAbrirMenuMobile = document.querySelector('#btn-menu-mobile');
